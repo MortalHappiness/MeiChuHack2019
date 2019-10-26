a771b37c3046a324c45a2eb72fd7b26025353dd2
yth98 <b06901031@ntu.edu.tw>
2019-10-26 14:26:00 +0800

    .

--- src/server/utils/crawler.py
+++ src/server/utils/crawler.py
@@ -13,14 +13,13 @@ counter = 0
 
 for device in dev_dict:
     #print("D "+device.get('id'))
-    response2 = requests.get("https://iot.cht.com.tw/iot/v1/device/"+device.get('id')+"/sensor", data=payload, headers=payloadHeader)
+    response2 = requests.get("https://iot.cht.com.tw/iot/v1/device/"+device.get('id')+"/rawdata", data=payload, headers=payloadHeader)
 
     sen_dict = json.loads(response2.text)
     for sensor in sen_dict:
         #print("S "+sensor.get('id'))
-        response3 = requests.get("https://iot.cht.com.tw/iot/v1/device/"+device.get('id')+"/sensor/"+sensor.get('id')+"/rawdata", data=payload, headers=payloadHeader)
-        print(json.loads(response3.text))
-        time.sleep(75e-3)
+        print(sensor)
         counter += 1
+    time.sleep(75e-3)
 
-print("Count: "+counter)
+print("Count: "+str(counter))
