0cae8c2c7c8d7d62c55fa5e285c0270351a73e77
MortalHappiness <b07901069@ntu.edu.tw>
2019-10-27 02:31:44 +0800

    complete sending 3D and 2D data to frond end

--- server.py
+++ server.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python3
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+
+from src.server.utils.data_loader import DataLoader
+# ===============================
+
 app = Flask(__name__, static_folder = 'static', template_folder = 'templates')
 app.config["JSON_AS_ASCII"] = False
+
+loader = DataLoader("./data", "roadlamp")
+
+# ===============================
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/position")
+def get_position_by_datatype():
+    datatype = request.args["type"]
+    return jsonify(loader.get_position_by_datatype(datatype))
+
+@app.route("/ndata")
+def get_n_lateset_data():
+    device_id = request.args["id"]
+    datatypes = request.args.getlist("type[]")
+    n = int(request.args["n"])
+    return jsonify(loader.get_n_lateset_data(device_id, datatypes, n))
 
 # To prevent caching
 @app.after_request
@@ -21,6 +41,7 @@ def add_header(r):
     r.headers['Cache-Control'] = 'public, max-age=0'
     return r
 
+# ================================
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=os.environ.get('PORT', 8080), debug=True)
---
+++ src/server/utils/data_loader.py
@@ -0,0 +1,129 @@
+import os
+import json
+
+# ==================================
+
+class DataLoader:
+    """
+    Data loader for sending data to frond end.
+    """
+    def __init__(self, datafolder, device_name):
+        with open(os.path.join(datafolder,
+                               device_name,
+                               'devices.json')) as fin:
+            devices = json.load(fin)
+
+        with open(os.path.join(datafolder,
+                               device_name,
+                               'sensors.json')) as fin:
+            sensors = json.load(fin)
+
+        with open(os.path.join(datafolder,
+                               device_name,
+                               'measurement.json')) as fin:
+            measurement = json.load(fin)
+
+        # auxiliary hash tables for quick search
+        devices_id_to_idx = dict()
+        for i in range(len(devices)):
+            devices_id_to_idx[devices[i]["id"]] = i
+
+        sensors_id_to_idx = dict()
+        for i in range(len(sensors)):
+            sensors_id_to_idx[sensors[i]["id"]] = i
+
+        sensor_id_to_timestamps = dict()
+        for data in measurement:
+            sensor_id_to_timestamps.setdefault(
+                data["sensor_id"], list()).append(data)
+
+        # mapping datatype to sensors id
+        datatypes = [x.split("_")[1] for x in devices[0]["sensors"]]
+        datatype_to_sensor_ids = dict()
+        for k in datatypes:
+            datatype_to_sensor_ids[k] = list()
+        for sensor in sensors:
+            k = sensor["id"].split("_")[1]
+            if k in datatypes:
+                datatype_to_sensor_ids[k].append(sensor["id"])
+
+        # store them as attributes
+        self.devices = devices
+        self.sensors = sensors
+        self.measurement = measurement
+        self.datatypes = datatypes
+        self.devices_id_to_idx = devices_id_to_idx
+        self.sensors_id_to_idx = sensors_id_to_idx
+        self.sensor_id_to_timestamps = sensor_id_to_timestamps
+        self.datatype_to_sensor_ids = datatype_to_sensor_ids
+
+    def get_position_by_datatype(self, datatype):
+        """
+        Arguments:
+            datatype(str): The datatype
+
+        Returns:
+            Array of information containing latitude, longitude,
+            and the measurement value.
+
+            [
+                {"lat": float,
+                 "lon": float,
+                 "value": int or float
+                }
+            ]
+        """
+        ans = list()
+        sensor_ids = self.datatype_to_sensor_ids[datatype]
+        for sensor_id in sensor_ids:
+            information = dict()
+            sensor_idx = self.sensors_id_to_idx[sensor_id]
+            sensor = self.sensors[sensor_idx]
+            device_id = sensor["device_id"]
+            device_idx = self.devices_id_to_idx[device_id]
+            device = self.devices[device_idx]
+            try:
+                information["lat"] = device["additional"]["lat"]
+                information["lon"] = device["additional"]["lon"]
+            except KeyError:
+                continue
+            timestamps = self.sensor_id_to_timestamps[sensor_id]
+            information["value"] = timestamps[-1]["value"]
+            ans.append(information)
+        return ans
+
+    def get_n_lateset_data(self, device_id, datatypes, n):
+        """
+        Arguments:
+            device_id(str): The device id
+            datatypes(list of str): list of datatypes
+            n(int): The number of latest data requested.
+
+        Returns:
+            Array containing datas in time order.
+
+            [
+                {
+                    key of datatypes: corresponding value
+                }
+            ]
+        """
+        for datatype in datatypes:
+            if datatype not in self.datatypes:
+                raise Exception("Invalid datatype query!")
+
+        device_idx = self.devices_id_to_idx[device_id]
+        device = self.devices[device_idx]
+        datatype_to_timestamps = dict()
+        for sensor_id in device["sensors"]:
+            datatype = sensor_id.split("_")[1]
+            if datatype in datatypes:
+                timestamps = self.sensor_id_to_timestamps[sensor_id]
+                datatype_to_timestamps[datatype] = timestamps[-n:]
+
+        ans = [dict() for _ in range(n)]
+        for i in range(n):
+            for k, v in datatype_to_timestamps.items():
+                ans[i][k] = v[i]["value"]
+
+        return ans
