bf755f56a1cc5886de2c2bc7ea74460884356818
yth98 <b06901031@ntu.edu.tw>
2019-10-26 14:02:31 +0800

    yeet.

---
+++ crawler/cht.py
@@ -0,0 +1,26 @@
+import json
+import requests
+import time
+
+payloadHeader = {'accept':'application/json', 'Content-Type': 'application/json', 'CK':'PKJ2FK5NBYFA1RCGG8'}
+
+payload = json.dumps({})
+response1 = requests.get("https://iot.cht.com.tw/iot/v1/device", data=payload, headers=payloadHeader)
+
+dev_dict = json.loads(response1.text)
+
+counter = 0
+
+for device in dev_dict:
+    #print("D "+device.get('id'))
+    response2 = requests.get("https://iot.cht.com.tw/iot/v1/device/"+device.get('id')+"/sensor", data=payload, headers=payloadHeader)
+
+    sen_dict = json.loads(response2.text)
+    for sensor in sen_dict:
+        #print("S "+sensor.get('id'))
+        response3 = requests.get("https://iot.cht.com.tw/iot/v1/device/"+device.get('id')+"/sensor/"+sensor.get('id')+"/rawdata", data=payload, headers=payloadHeader)
+        print(json.loads(response3.text))
+        #time.sleep(80e-3)
+        counter += 1
+
+print("Count: "+counter)
